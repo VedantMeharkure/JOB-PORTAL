541fc8cc04e3f71d1ac8df41159684ab47d34e3a
Vedant Meharkure <vedantmeharkure@gmail.com>
2026-09-01 17:28:46 +0530

    prepare backend for vercel

--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
 require("dotenv").config();
-const connectDB =require("./config/db.js");
-const app =require("./app.js")
-const PORT=process.env.PORT || 8000;
+
+const connectDB = require("./config/db.js");
+const app = require("./app.js");
+
 connectDB();
-app.listen(PORT,() => console.log(`Server connected to the Port:${PORT}`));
+
+module.exports = app;
